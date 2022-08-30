7e26302fc4bfba1b731e209b42c2de25bcb35258
Phanuphong Machai <phanuphong.forwork@gmail.com>
2022-08-30 22:01:29 +0700

    feat: first init

---
+++ layouts/default.vue
@@ -0,0 +1,131 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item class="d-lg-none d-lg-flex">
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            ระบบทะเบียนครัวเรือน
+          </v-list-item-title>
+          <!-- <v-list-item-subtitle> subtext </v-list-item-subtitle> -->
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list shaped nav>
+        <v-subheader>REPORTS</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="primary" dark dense app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <!-- <v-toolbar-title>ระบบทะเบียนครัวเรือน</v-toolbar-title> -->
+
+      <v-spacer></v-spacer>
+
+      <v-menu bottom left class="d-md-none d-lg-flex">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" depressed v-on="on">
+            <v-icon left> mdi-account-circle-outline </v-icon>
+            Admin
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                John Leider
+              </v-list-item-title>
+              <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-icon>mdi-menu-down</v-icon>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+        <v-list nav dense>
+          <v-list-item-group v-model="selectedItem" color="primary">
+            <v-list-item v-for="(item, i) in lists" :key="i">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <Breadcrumbs class="mt-6" />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="text-h4"> Title </v-col>
+        </v-row>
+
+        <!-- If using vue-router -->
+        <Nuxt></Nuxt>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Breadcrumbs from "@/components/Breadcrumbs";
+
+export default {
+  components: {
+    Breadcrumbs,
+  },
+  data() {
+    return {
+      drawer: true,
+
+      lists: [{ text: "Log Out", icon: "mdi-logout" }],
+      selectedItem: 1,
+      items: [
+        { text: "Real-Time", icon: "mdi-clock" },
+        { text: "Audience", icon: "mdi-account" },
+        { text: "Conversions", icon: "mdi-flag" },
+      ],
+    };
+  },
+
+  watch: {},
+};
+</script>
