dcbcf65bae877a2d8e2f0c58868e52a42e7739c3
M i c r o w a v e <70051626+phanuphongforwork@users.noreply.github.com>
2022-09-02 16:08:56 +0700

    feat: login page (#1)

---
+++ layouts/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app
+    style="background: linear-gradient(rgb(56, 142, 60), rgb(102, 187, 106))"
+  >
+    <v-container class="fill-height">
+      <v-row align="center" justify="center" class="fill-height">
+        <Nuxt />
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
